661878b138340ab82bea59024c5cdef4a30471aa
Gregorio Martinez <greg@gmartinez.com>
2016-05-31 08:58:50 -0400

    Move wiki functions to wikipedia file. Move caching to below comment actually poste

--- redditbot.go
+++ redditbot.go
@@ -50,13 +50,6 @@ func main() {
 				continue
 			}
 
-			// Store a small cache of comments if we have space
-			if len(commented) < commentLimit {
-				commented = append(commented, id)
-			} else {
-				commented = make([]string, 0, 1)
-			}
-
 			link, lang, query, section, total := extractWikiLink(listing.Data.Body)
 
 			if total > 0 {
@@ -112,55 +105,17 @@ func main() {
 				commentparams["parent"] = id
 				postNewComment(client, commentparams)
 				log.Printf("Posted comment in /r/%s with parent id: %s \n", sub, id)
+
+				// Store a small cache of comments if we have space
+				if len(commented) < commentLimit {
+					commented = append(commented, id)
+				} else {
+					commented = make([]string, 0, 1)
+				}
 			}
 		}
 		time.Sleep(3 * time.Second)
 	}
-}
-
-func wikiGetSection(lang string, query string, sectionNum int64) (WikipediaRevisionResponse, error) {
-	var wikiRevisionSection WikipediaRevisionResponse
-
-	url := fmt.Sprintf(`https://%s.wikipedia.org/w/api.php?action=query&prop=revisions&titles=%s&rvprop=content&rvsection=%d&formatversion=2&rvlimit=1&rvparse&format=json`, lang, query, sectionNum)
-	resp, err := http.Get(url)
-	if err != nil {
-		return wikiRevisionSection, err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return wikiRevisionSection, err
-	}
-
-	err = json.Unmarshal(body, &wikiRevisionSection)
-	if err != nil {
-		return wikiRevisionSection, err
-	}
-
-	return wikiRevisionSection, nil
-}
-
-func wikiGetAllSections(lang string, query string) (WikipediaSectionResponse, error) {
-	var wikiSections WikipediaSectionResponse
-
-	url := fmt.Sprintf(`https://%s.wikipedia.org/w/api.php?action=parse&page=%s&prop=sections&format=json&formatversion=2`, lang, query)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return wikiSections, err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return wikiSections, err
-	}
-
-	err = json.Unmarshal(body, &wikiSections)
-	if err != nil {
-		return wikiSections, err
-	}
-
-	return wikiSections, nil
 }
 
 func formatComment(extract string, title string, url string) (string, error) {
@@ -207,53 +162,6 @@ func canPost(poster string, sub string, id string, blacklistSubs []string, black
 	}
 
 	return true
-}
-
-func extractWikiSection(s string) string {
-
-	r := regexp.MustCompile(`<p>.+?(?:\n+)?</p>`)
-	tmp := strings.Join(r.FindAllString(s, -1), "")
-
-	re := regexp.MustCompile(`\[.+?\]`)
-	extract := re.ReplaceAllLiteralString(tmp, "")
-
-	return sanitize.HTML(extract)
-}
-
-func extractWikiLink(url string) (string, string, string, string, int) {
-	// RegEx for finding wikipedia links
-	r := regexp.MustCompile(`http(?:s)?://([a-zA-Z]{2}).(?:m\.)?wikipedia.org/wiki/([^\s|#]+)(?:#(.+))?`)
-
-	matches := r.FindStringSubmatch(url)
-
-	if len(matches) > 0 {
-		link, lang, url, section, total := matches[0], matches[1], matches[2], matches[3], len(matches)
-		return link, lang, url, section, total
-	}
-
-	return "", "", "", "", 0
-
-}
-
-func wikiData(link string) WikipediaResponse {
-	resp, err := http.Get(link)
-	if err != nil {
-		panic(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var wiki WikipediaResponse
-
-	err = json.Unmarshal(body, &wiki)
-	if err != nil {
-		panic(err)
-	}
-
-	return wiki
 }
 
 func contains(s []string, b string) bool {
--- wikipedia.go
+++ wikipedia.go
@@ -53,3 +53,95 @@ type WikipediaRevisionResponse struct {
 		} `json:"pages"`
 	} `json:"query"`
 }
+
+func wikiGetSection(lang string, query string, sectionNum int64) (WikipediaRevisionResponse, error) {
+	var wikiRevisionSection WikipediaRevisionResponse
+
+	url := fmt.Sprintf(`https://%s.wikipedia.org/w/api.php?action=query&prop=revisions&titles=%s&rvprop=content&rvsection=%d&formatversion=2&rvlimit=1&rvparse&format=json`, lang, query, sectionNum)
+	resp, err := http.Get(url)
+	if err != nil {
+		return wikiRevisionSection, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return wikiRevisionSection, err
+	}
+
+	err = json.Unmarshal(body, &wikiRevisionSection)
+	if err != nil {
+		return wikiRevisionSection, err
+	}
+
+	return wikiRevisionSection, nil
+}
+
+func wikiGetAllSections(lang string, query string) (WikipediaSectionResponse, error) {
+	var wikiSections WikipediaSectionResponse
+
+	url := fmt.Sprintf(`https://%s.wikipedia.org/w/api.php?action=parse&page=%s&prop=sections&format=json&formatversion=2`, lang, query)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return wikiSections, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return wikiSections, err
+	}
+
+	err = json.Unmarshal(body, &wikiSections)
+	if err != nil {
+		return wikiSections, err
+	}
+
+	return wikiSections, nil
+}
+
+func extractWikiSection(s string) string {
+
+	r := regexp.MustCompile(`<p>.+?(?:\n+)?</p>`)
+	tmp := strings.Join(r.FindAllString(s, -1), "")
+
+	re := regexp.MustCompile(`\[.+?\]`)
+	extract := re.ReplaceAllLiteralString(tmp, "")
+
+	return sanitize.HTML(extract)
+}
+
+func extractWikiLink(url string) (string, string, string, string, int) {
+	// RegEx for finding wikipedia links
+	r := regexp.MustCompile(`http(?:s)?://([a-zA-Z]{2}).(?:m\.)?wikipedia.org/wiki/([^\s|#]+)(?:#(.+))?`)
+
+	matches := r.FindStringSubmatch(url)
+
+	if len(matches) > 0 {
+		link, lang, url, section, total := matches[0], matches[1], matches[2], matches[3], len(matches)
+		return link, lang, url, section, total
+	}
+
+	return "", "", "", "", 0
+
+}
+
+func wikiData(link string) WikipediaResponse {
+	resp, err := http.Get(link)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var wiki WikipediaResponse
+
+	err = json.Unmarshal(body, &wiki)
+	if err != nil {
+		panic(err)
+	}
+
+	return wiki
+}
